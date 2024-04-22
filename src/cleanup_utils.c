2e5ce34692370ea509254a3c1c1fe8acaebe984c
Fred Rukundo <frukundo@e1r5p5.1337.ma>
2024-04-23 00:05:27 +0100

    cleanup

---
+++ src/cleanup_utils.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frukundo <frukundo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 23:33:05 by frukundo          #+#    #+#             */
+/*   Updated: 2024/04/22 23:33:31 by frukundo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/cub3d.h"
+
+/*
+	@desc:
+	-  to free the memory allocated for animation images stored in a linked list
+*/
+void	free_animation(t_game *g, t_list *start)
+{
+	t_list	*temp;
+
+	temp = NULL;
+	while (start)
+	{
+		temp = start;
+		start = start->next;
+		if (g && ((t_img *)temp->content)->i)
+			mlx_destroy_image(g->mlx_ptr, ((t_img *)temp->content)->i);
+		free(temp->content);
+		free(temp);
+	}
+}
+
+/*
+	@desc:
+	- used to free all the images and resources related to the game
+*/
+void	destroy_images(t_game *g)
+{
+	free_animation(g, g->tex.n_bak);
+	free_animation(g, g->tex.s_bak);
+	free_animation(g, g->tex.e_bak);
+	free_animation(g, g->tex.w_bak);
+	if (g->tex.b->i)
+		mlx_destroy_image(g->mlx_ptr, g->tex.b->i);
+	if (g->win_img.i)
+		mlx_destroy_image(g->mlx_ptr, g->win_img.i);
+	if (g->win_g.i)
+		mlx_destroy_image(g->mlx_ptr, g->win_g.i);
+	if (g->win_r.i)
+		mlx_destroy_image(g->mlx_ptr, g->win_r.i);
+	if (g->scope->i)
+		mlx_destroy_image(g->mlx_ptr, g->scope->i);
+	if (g->win_ptr)
+		mlx_destroy_window(g->mlx_ptr, g->win_ptr);
+	if (g->minimap.i)
+		mlx_destroy_image(g->mlx_ptr, g->minimap.i);
+	if (g->miniview.i)
+		mlx_destroy_image(g->mlx_ptr, g->miniview.i);
+	free(g->tex.b);
+	free(g->scope);
+}
+
+/*
+	@desc:
+	- cleanup function called at the end of the game or when exiting the program
+*/
+void	cub_end(t_game *g)
+{
+	if (!g)
+		return ;
+	ft_free_matrix(&g->map);
+	if (g->fd > 0)
+		close(g->fd);
+	destroy_images(g);
+}
