0a5e257c4d2791764e4795a8b7b3718bc2074ef9
Fred Rukundo <frukundo@e1r5p5.1337.ma>
2024-04-22 18:43:15 +0100

    raycasting

--- src/raycast.c
+++ src/raycast.c
@@ -12,6 +12,16 @@
 
 #include "../inc/cub3d.h"
 
+/*
+	@desc:
+	- g->ray.angle: Sets the initial angle for raycasting based on the player's
+		direction (S, W, N).The angles are set to 90, 180, or 270 degrees, respectively.
+	- g->ray.hfov: Sets the half field of view (FOV) to 30 degrees.
+	- g->ray.incre_angle: Angle difference between one ray and the next one
+		(2 * HFOV / window_width)
+	- g->ray.precision: Sets the Size of 'steps' taken every iteration.
+	- g->ray.lim: Sets a Limit of the distance the player can view.
+*/
 void	init_ray(t_game *g)
 {
 	g->ray.angle = 0;
@@ -27,6 +37,25 @@ void	init_ray(t_game *g)
 	g->ray.lim = 11;
 }
 
+/*
+	@desc:
+	- From the the player's position, we move the ray forward incrementing the x's and y's coordinates of the ray.
+		(ray.x += ray_cos; ray.y += ray_sin;) where :ray_cos = cos(degree_to_radians(ray_angle)) / g->ray.precision;
+		ray_sin = sin(degree_to_radians(ray_angle)) / g->ray.precision;
+	- Repeat this step until we reach the limit or we hit a wall.
+	- Calculate the distance between the player's and the ray's position using the euclidean distance
+		( distance = sqrt(powf(x - pl.x - 0.5, 2.) + powf(y - pl.y - 0.5, 2.)); )
+	- Fix fisheye(distance = distance * cos(degree_to_radians(ray_angle - g->ray.angle)))
+	- distance is really helpful to calculate the height of the wall height:
+		( wall_height = (window_height / (1.5 * distance)); )
+	NB:
+	Depending on what the ray hits:
+	- If it hits a wall ('1' or 'c' in the map), a red pixel is drawn on the minimap.
+	- If it's close to the player's viewing direction, a green pixel is drawn.
+	- Otherwise, a light blue pixel is drawn.
+	- Finally, a yellow square is drawn on the minimap to represent the player's position.
+
+*/
 float	distance_to_wall(t_game *g, float ray_angle)
 {
 	float	d;
@@ -54,6 +83,14 @@ float	distance_to_wall(t_game *g, float ray_angle)
 	return (d * cos(degree_to_radians(ray_angle - g->ray.angle)));
 }
 
+/*
+	@desc:
+	- ray_angle: Starts from the leftmost side of the FOV and
+		iteratively increases to cover the entire horizontal FOV.
+	- ray_count: Counts the number of rays processed.
+	- dist: Calculates the distance to the nearest wall for each ray.
+	- then cub_draw function is called to draw the rays on the screen.
+*/
 void	cub_raycast(t_game *g)
 {
 	float	ray_angle;
